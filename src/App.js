00cc9c36c055af31156e3bb5764022bc78fdbb26
gokkulsrinivasan <gokkulsrinivasan11@gmail.com>
2024-10-15 00:54:20 +0530

    Initial commit of project files and .gitignore

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar.js";
+import Search  from "./components/Search.js";
+import Products from "./components/Products.js";
+import About from "./components/About.js";
+import Footer from "./components/footer.js";
+
+function App(){
+    return(
+        <div>
+               <Navbar></Navbar>
+    <Search></Search>
+    <Products></Products>
+    <About></About>
+    <Footer></Footer>
+        </div>
+    )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import react from "react"
+import ReactDOM from "react-dom/client";
+
+import "./style.css"
+
+
+
+import App from "./App.js"
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+// Creating Navbar component
+
+
+//search component
+
+
+//products component
+
+
+
+//about component
+
+
+//footer component
+
+
+root.render(<App></App>)
